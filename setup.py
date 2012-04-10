5e511b3510fd274678e264f7c848e23b35abf7e2
Eduard Carreras <ecarreras@gisce.net>
2012-04-10 08:34:39 +0000

    Adding distribution support

---
+++ setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Setup file for arquia client.
+"""
+
+from distutils.core import setup
+
+PACKAGES = ['arquiapgw', ]
+PACKAGES_DATA = {}
+
+setup(name='arquiapgw',
+      description = """A client to submit payment orders to the Arquia
+      service.""",
+      author='GISCE Enginyeria',
+      author_email='devel@gisce.net',
+      url='http://www.gisce.net',
+      version='0.1.0',
+      license='General Public Licence 2',
+      long_description='''Long description''',
+      provides=['arquiapgw'],
+      install_requires=['PyDES'],
+      packages=PACKAGES,
+      package_data=PACKAGES_DATA,
+      scripts=[],
+)
